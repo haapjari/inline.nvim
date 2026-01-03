ada8f0f5ea4b5aeb083f100bbca3f5be6fdea87a
Jari Haapasaari <haapjari@gmail.com>
2026-01-03 06:59:12 +0200

    chore: release v0.0.6

--- lua/inline/init.lua
+++ lua/inline/init.lua
@@ -11,7 +11,7 @@
 ---@field show_config fun(): nil Display current configuration
 local M = {}
 
-M.version = "0.0.5"
+M.version = "0.0.6"
 
 --------------------------------------------------------------------------------
 -- Constants
